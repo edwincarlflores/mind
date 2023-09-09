b42a6cdf02451017b6c3e6e50c5191451e997d99
Edwin Carl Flores <edwincarlflores@gmail.com>
2023-09-10 04:46:27 +1200

    Remove unnecessary variables

--- db/connection.go
+++ db/connection.go
@@ -20,38 +20,3 @@ func CreateDBConnection() *sqlx.DB {
 
 	return db
 }
-
-type Thought struct {
-	ID          int
-	Body        string
-	Description string
-	Kind        string
-	Public      bool
-}
-
-var Thoughts = []Thought{
-	{
-		ID:          1,
-		Body:        "Initial thought",
-		Description: "Test tought",
-		Public:      false,
-	},
-	{
-		ID:          2,
-		Body:        "Second thought",
-		Description: "Test tought",
-		Public:      true,
-	},
-	{
-		ID:          3,
-		Body:        "Third thought",
-		Description: "Test tought",
-		Public:      true,
-	},
-	{
-		ID:          4,
-		Body:        "Fourth thought",
-		Description: "Test tought",
-		Public:      false,
-	},
-}
